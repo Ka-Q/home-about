d41031d7ca376185546b0ef28319e98a664072ac
Ka-Q <akulaurila@gmail.com>
2024-01-07 15:41:28 +0200

    Fixed accessibility for SkillCards

--- src/components/skillCard/skillCard.jsx
+++ src/components/skillCard/skillCard.jsx
@@ -3,8 +3,8 @@ import styles from "./skillCard.module.css"
 const SkillCard = ({category}) => {
     return (
         <div key={category.title} className={styles.container}>
-            <h3 className={styles.title}>{category.title}</h3>
-            {category.skills.join(', ')}
+            <h2 className={styles.title}>{category.title}</h2>
+            <p>{category.skills.join(', ')}</p>
         </div>
     )
 }
