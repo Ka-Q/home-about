ef1a6f8427ebbcaa6788d563c38f883353896340
Ka-Q <akulaurila@gmail.com>
2024-01-08 21:22:17 +0200

    Added a loading page

---
+++ src/app/loading.jsx
@@ -0,0 +1,21 @@
+import styles from "./loading.module.css";
+
+const LoadingPage = () => {
+    console.log("laoding...");
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.text}>Loading</h1>
+            <div className={styles.loading_container}>
+                <h1 className={styles.loading}>
+                    <span className={styles.dot1}>.</span>
+                    <span className={styles.dot2}>.</span>
+                    <span className={styles.dot3}>.</span>
+                    <span className={styles.dot4}>.</span>
+                    <span className={styles.dot5}>.</span>
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default LoadingPage;
