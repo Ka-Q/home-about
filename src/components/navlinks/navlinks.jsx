a483fa0bc8b3a83c934ccfda077cedc0550c08c2
Ka-Q <akulaurila@gmail.com>
2024-01-20 01:55:38 +0200

    Added a navlink for all projects and a responsive divider

--- src/components/navlinks/navlinks.jsx
+++ src/components/navlinks/navlinks.jsx
@@ -19,6 +19,14 @@ const links = [
         text: "Contact",
         path: "/#contact"
     },
+    {
+        text: "|",
+        path: "div"
+    },
+    {
+        text: "All projects",
+        path: "/projects"
+    }
 ]
 
 const Navlinks = () => {
@@ -27,7 +35,7 @@ const Navlinks = () => {
         <div className={styles.container}>
         {links.map((n)=> {
             return (
-                <Link href={n.path} key={n.path}>{n.text}</Link>
+                <>{n.path != "div"? <Link href={n.path} key={n.path}>{n.text}</Link> : <div className={styles.link_divider}></div>}</>
             )
         })}
         </div>
